d8b4b4ce232aedad3badfb604f6119f6b015419a
Taljjaa <jenken131@gmail.com>
2020-05-30 11:12:42 -0700

    added basic blog pages and index page

---
+++ src/pages/blogpost.md
@@ -0,0 +1,6 @@
+---
+title: "Blog Post"
+date: "2019-08-10"
+---
+
+This is a cool blog post
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,37 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  console.log(data)
+  return (
+    <div>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
