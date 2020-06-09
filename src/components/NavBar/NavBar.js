d07ec27794ddfb243258a450e0509397a416890a
Taljjaa <jenken131@gmail.com>
2020-06-08 20:03:22 -0700

    added react icons to nav bar, general nav bar styling, and added 404 page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,25 @@
 import React from "react"
 import "./NavBar.scss"
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai"
+import { IconContext } from "react-icons"
 
 export default function NavBar() {
   return (
     <nav>
-      <a>Home</a>
-      <a>About</a>
-      <a>All Posts</a>
+      <ul className="menu">
+        <li className="logo">
+          <a>Home</a>
+        </li>
+        <div className="secondMenu">
+          <li className="item">
+            <a>All Posts</a>
+          </li>
+          <IconContext.Provider value={{ className: "icon" }}>
+            <AiFillGithub />
+            <AiOutlineTwitter />
+          </IconContext.Provider>
+        </div>
+      </ul>
     </nav>
   )
 }
