e483f2b110cd74dbcac155ff48ac7bfc47f1004f
Taljjaa <jenken131@gmail.com>
2020-06-09 21:57:48 -0700

    added styling for the individual blog posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
+import "./blogPost.scss"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <>
-      <div>
+      <div className="blogPostContent">
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
