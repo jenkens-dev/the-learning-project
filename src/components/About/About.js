b35e8ea339322b07e7add0dc35fc62756e78a1e5
Taljjaa <jenken131@gmail.com>
2020-06-16 12:54:49 -0700

    fixed broken links, made emojis more accessible, and fixed SEO query

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,14 +5,26 @@ import { Link } from "gatsby"
 export default function About() {
   return (
     <div className="aboutSection">
-      <h2 className="firstLine">Hello and welcome to my digital garden! 👩‍🌾</h2>
+      <h2 className="firstLine">
+        Hello and welcome to my digital garden!{" "}
+        <span role="img" aria-label="farmer">
+          👩‍🌾
+        </span>
+      </h2>
       <p>
         My name is Jen Kennedy and I'm a Full-Stack developer. This digital
         garden is a place where I can take notes, share my thoughts about things
         I'm learning, and document my programming progress. The articles here
-        are either finished (🌻), or in progress (🌱) and are being pruned and
-        updated as I learn more. To read more about digital gardens check out my{" "}
-        <Link to="/digital-garden">article</Link>!
+        are either finished (
+        <span role="img" aria-label="sunflower">
+          🌻
+        </span>
+        , or in progress (
+        <span role="img" aria-label="seedling">
+          🌱
+        </span>
+        ) and are being pruned and updated as I learn more. To read more about
+        digital gardens check out my <Link to="/digital-garden">article</Link>!
       </p>
     </div>
   )
