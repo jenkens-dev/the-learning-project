5f888148be51ca28b9c7baae2d4688943d13babf
Taljjaa <jenken131@gmail.com>
2020-06-29 12:49:43 -0700

    fixed post-page styling

--- src/components/posts-page-layout.js
+++ src/components/posts-page-layout.js
@@ -2,20 +2,23 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import CodeBlock from "../components/CodeBlock"
-
-const shortcodes = { Link, CodeBlock }
+import "./blogPost.scss"
 
 const components = {
   code: CodeBlock,
+  wrapper: props => (
+    <div className="blogPost">
+      <main {...props} />
+    </div>
+  ),
 }
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
     <Layout>
-      <h1>{mdx.frontmatter.title}</h1>
+      <h1 className="title">{mdx.frontmatter.title}</h1>
       <MDXProvider components={components}>
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </MDXProvider>
