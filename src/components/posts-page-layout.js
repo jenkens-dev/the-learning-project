d52d665e91f0bf8955647540c6d809a1503f72d8
Taljjaa <jenken131@gmail.com>
2020-06-29 11:47:35 -0700

    added layout to post page

--- src/components/posts-page-layout.js
+++ src/components/posts-page-layout.js
@@ -3,17 +3,18 @@ import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
-const shortcodes = { Link } 
+const shortcodes = { Link }
 
 export default function PageTemplate({ data: { mdx } }) {
   return (
-    <div>
+    <Layout>
       <h1>{mdx.frontmatter.title}</h1>
       <MDXProvider components={shortcodes}>
         <MDXRenderer>{mdx.body}</MDXRenderer>
       </MDXProvider>
-    </div>
+    </Layout>
   )
 }
 
