600903a92944d638afbbad3ee5bf86163af957a2
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2021-01-05 19:33:48 -0800

    Updated About Me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,9 +12,10 @@ export default function About() {
         </span>
       </h2>
       <p>
-        My name is Jen Kennedy and I'm a frontend developer. This digital
-        garden is a place where I can take notes, share what I'm learning, and document my programming progress. The articles here
-        are either finished (
+        My name is Jen Kennedy and I'm a frontend developer. I currently live in Seattle, WA 🌲! I love frontend development and enjoy working on products and making designs and features come to life. Some of my favorite technologies are React, TypeScript, GraphQL, and Redux. I also enjoy leveling up my skills in CSS and testing. When I'm not programming I enjoy playing with my dog, reading manga, teaching myself Japanese, and playing videogames. And to learn more about me, feel free to read my article about my <Link to="/blog/my-journey-into-tech">journey into tech</Link>.
+      </p>
+        <p>
+        This digital garden is a place where I can take notes, share what I'm learning, and document my programming progress. The articles here are either finished (
         <span role="img" aria-label="sunflower">
           🌻
         </span>
@@ -24,7 +25,7 @@ export default function About() {
         </span>
         ) and are being pruned and updated as I learn more! To read more about
         digital gardens check out my{" "}
-        <Link to="/blog/digital-garden">article</Link>! And to learn more about me, feel free to read my article about my <Link to="/blog/my-journey-into-tech">journey into tech</Link>.
+        <Link to="/blog/digital-garden">article</Link>! 
       </p>
     </div>
   )
