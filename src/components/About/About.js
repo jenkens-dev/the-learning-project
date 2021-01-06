6dd7f46500b6dd8a45af488d95a6293429315c6a
Jennifer Kennedy <jenniferkennedy@Jennifers-MacBook-Pro.local>
2021-01-05 19:06:54 -0800

    added article about journey into tech

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,9 +12,8 @@ export default function About() {
         </span>
       </h2>
       <p>
-        My name is Jen Kennedy and I'm a Full-Stack developer. This digital
-        garden is a place where I can take notes, share my thoughts about things
-        I'm learning, and document my programming progress. The articles here
+        My name is Jen Kennedy and I'm a frontend developer. This digital
+        garden is a place where I can take notes, share what I'm learning, and document my programming progress. The articles here
         are either finished (
         <span role="img" aria-label="sunflower">
           🌻
@@ -23,9 +22,9 @@ export default function About() {
         <span role="img" aria-label="farmer">
           👩‍🌾
         </span>
-        ) and are being pruned and updated as I learn more. To read more about
+        ) and are being pruned and updated as I learn more! To read more about
         digital gardens check out my{" "}
-        <Link to="/blog/digital-garden">article</Link>!
+        <Link to="/blog/digital-garden">article</Link>! And to learn more about me, feel free to read my article about my <Link to="/blog/my-journey-into-tech">journey into tech</Link>.
       </p>
     </div>
   )
