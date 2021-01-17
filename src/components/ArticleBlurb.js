e7fb9f10c792acc2ddc68dcc6a14f63148af89a0
Jennifer Kennedy <jenken131@gmail.com>
2021-01-17 15:22:28 -0800

    updated spacing for all posts page

--- src/components/ArticleBlurb.js
+++ src/components/ArticleBlurb.js
@@ -6,11 +6,14 @@ export default function ArticleBlurb({ node }) {
     <div key={node.id}>
       <Link to={node.fields.slug}>
         <div className="flex items-center">
-          <h3 className="text-xl font-semibold">{node.frontmatter.title}</h3>
-          <span> — {node.frontmatter.date}</span>
+          <h3 className="text-lg font-semibold">{node.frontmatter.title}</h3>
+          <span className="whitespace-pre font-light">
+            {" "}
+            — {node.frontmatter.date}
+          </span>
           <span>: {node.frontmatter.status}</span>
         </div>
-        <p>{node.excerpt}</p>
+        <p className="p-2">{node.excerpt}</p>
       </Link>
     </div>
   )
