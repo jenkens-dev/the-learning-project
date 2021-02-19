b426ff28a406970a0a3987b9e5f9c45efc1fa276
Jennifer Kennedy <jenken131@gmail.com>
2021-02-19 12:07:02 -0800

    added responsiveness to layout page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,12 @@ import Footer from "./Footer"
 export default function Layout({ children }) {
   return (
     <div className="bg-green-50 min-h-screen">
-      <div className="max-w-6xl flex flex-col my-0 mx-auto min-h-screen">
+      <div className="flex flex-col min-h-screen">
         <SEO />
         <NavBar />
-        <div className="my-0 mx-auto max-w-3xl flex-grow">{children}</div>
+        <div className="flex-grow max-w-xs mx-auto my-0 sm:max-w-3xl">
+          {children}
+        </div>
         <Footer />
       </div>
     </div>
