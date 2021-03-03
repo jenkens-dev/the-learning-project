cfe0ae581daed62eb5280a85338113115c5f57ee
Jennifer Kennedy <jenken131@gmail.com>
2021-03-02 16:29:43 -0800

    removed unused code from SEO and added SEO component to index

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -1,11 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
-import { useLocation } from "@reach/router"
 import { useStaticQuery, graphql } from "gatsby"
 
 const SEO = ({ title, description, image, article, pathname }) => {
-  // const { pathname } = useLocation()
   const { site } = useStaticQuery(query)
 
   const {
