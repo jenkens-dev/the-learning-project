1ba933e33d1c1688b6f44ac7bce17081c9d29f1d
Jennifer Kennedy <jenken131@gmail.com>
2021-03-03 15:00:49 -0800

    comment out filesystem data code until solution is found

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,13 +14,13 @@ module.exports = {
         path: `${__dirname}/content/posts/`,
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `./src/data/`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `data`,
+    //     path: `./src/data/`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
