db68d9c3ec78ce6d52f97edf6e1f336e7e4d8a94
Jennifer Kennedy <jenken131@gmail.com>
2021-03-20 14:55:01 -0700

    added resume link

--- src/components/About.js
+++ src/components/About.js
@@ -12,19 +12,26 @@ const About = () => {
       <p className="mb-5">
         My name is Jen Kennedy and I'm a frontend developer. I currently live in
         Seattle, WA <Emoji symbol="🌲" label="pine tree emoji" />! I love
-        frontend development and enjoy working on products and making designs
-        and features come to life. Some of my favorite technologies are React,
-        TypeScript, GraphQL, and Redux. I also enjoy leveling up my skills in
-        CSS and testing. When I'm not programming I enjoy playing with my dog,
-        reading manga, teaching myself Japanese, and playing videogames. And to
-        learn more about me, feel free to read my{" "}
+        frontend development and enjoy working on interesting products with
+        unique challenge. Making designs and features come to life is my
+        favorite part. Some of my favorite technologies are React, TypeScript,
+        GraphQL, and Redux. I also enjoy leveling up my skills in CSS and
+        testing. When I'm not programming I enjoy playing with my dog, reading
+        manga, teaching myself Japanese, and playing videogames. And to learn
+        more about me, feel free to read my{" "}
         <Link
           className="linkUnderline dark:darkUnderline"
           to="/blog/my-journey-into-tech"
         >
           article about my journey into tech
+        </Link>{" "}
+        or{" "}
+        <Link
+          className="linkUnderline dark:darkUnderline"
+          to="https://docs.google.com/document/d/1jg6qBShEtZ4OMEPJe33Pr2EEyi7ctY4AkveQI9DGIpg/edit?usp=sharing"
+        >
+          check out my resume.
         </Link>
-        .
       </p>
       <p className="mb-5">
         This digital garden is a place where I can take notes, share what I'm
