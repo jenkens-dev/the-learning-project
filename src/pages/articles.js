87337f8321330dd44a69a9aba3385ec459573594
Jennifer Kennedy <jenken131@gmail.com>
2021-08-05 16:12:34 -0700

    removed tag logic

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -8,12 +8,6 @@ const Articles = ({ data }) => {
   const [search, setSearch] = useState("")
   let articles = data.allMdx.edges
 
-  const allTagsWithDups = articles.map(article => {
-    return article.node.frontmatter.tags
-  })
-
-  const tags = [...new Set(allTagsWithDups.flat(Infinity))]
-
   const handleChange = e => {
     e.preventDefault()
     setSearch(e.target.value)
@@ -41,9 +35,6 @@ const Articles = ({ data }) => {
           Search articles
         </label>
       </div>
-      {tags.map(tag => {
-        return <button className="bg-white m-2 rounded p-2">{tag}</button>
-      })}
       {articles.length === 0 ? (
         <div className="text-3xl text-green-900 dark:text-green-400">
           Sorry no articles found!
