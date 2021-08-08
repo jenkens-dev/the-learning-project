40fa87945d2207ce667a026edd688c00f459d4fe
Jennifer Kennedy <jenken131@gmail.com>
2021-08-08 13:32:47 -0700

    rdestructured node prop for articleblurb component

--- src/components/ArticleBlurb.js
+++ src/components/ArticleBlurb.js
@@ -1,22 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const ArticleBlurb = ({ node }) => {
+const ArticleBlurb = ({ node: { id, fields, frontmatter, excerpt } }) => {
   return (
     <div
       className="my-4 bg-white rounded p-5 shadow-green transform transition duration-500 hover:scale-105 dark:text-black"
-      key={node.id}
+      key={id}
     >
-      <Link to={node.fields.slug}>
+      <Link to={fields.slug}>
         <div className="flex flex-wrap">
-          <h3 className="text-lg font-semibold">{node.frontmatter.title}</h3>
+          <h3 className="text-lg font-semibold">{frontmatter.title}</h3>
           <span className="whitespace-pre font-light">
             {" "}
-            — {node.frontmatter.date}
+            — {frontmatter.date}
           </span>
-          <span>: {node.frontmatter.status}</span>
+          <span>: {frontmatter.status}</span>
         </div>
-        <p className="text-gray-500">{node.excerpt}</p>
+        <p className="text-gray-500">{excerpt}</p>
       </Link>
     </div>
   )
