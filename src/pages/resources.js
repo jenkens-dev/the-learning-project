90ba16e3f3cc1097852c3d303f4083b8b0b12bbf
Jen Kennedy <jenken131@gmail.com>
2021-10-20 15:43:34 -0700

    created ResourcesPage skeleton

---
+++ src/pages/resources.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const ResourcesPage = () => {
+  return (
+    <Layout>
+      <SEO title="Resources" tagline="A collection of my favorite resources"/>
+      <h1>Resources</h1>
+    </Layout>
+  )
+}
+
+export default ResourcesPage
