19a838cae07b6217b0bea931f8267b2f379beceb
Jen Kennedy <jenken131@gmail.com>
2021-11-04 13:03:55 -0700

    used article tag instead of div for articleblurb component and started semantic html article

--- src/components/ArticleBlurb.js
+++ src/components/ArticleBlurb.js
@@ -3,13 +3,13 @@ import { Link } from "gatsby"
 
 const ArticleBlurb = ({ node: { id, fields, frontmatter, excerpt } }) => {
   return (
-    <div
+    <article
       className="my-4 bg-white rounded p-5 shadow-green transform transition duration-500 hover:scale-105 dark:text-black"
       key={id}
     >
       <Link to={fields.slug}>
         <div className="flex flex-wrap">
-          <h3 className="text-lg font-semibold">{frontmatter.title}</h3>
+          <h2 className="text-lg font-semibold">{frontmatter.title}</h2>
           <span className="whitespace-pre font-light">
             {" "}
             — {frontmatter.date}
@@ -18,7 +18,7 @@ const ArticleBlurb = ({ node: { id, fields, frontmatter, excerpt } }) => {
         </div>
         <p className="text-gray-500">{excerpt}</p>
       </Link>
-    </div>
+    </article>
   )
 }
 
