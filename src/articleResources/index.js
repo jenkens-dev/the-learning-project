1b2a9b1c2335e6359370c7e20546dcc9c119b8f0
Jen Kennedy <jenken131@gmail.com>
2021-11-04 13:26:50 -0700

    added resources to their own file to clean up resources page file

---
+++ src/articleResources/index.js
@@ -0,0 +1,34 @@
+export const ReactResources = {
+  topic: "React",
+  articles: [
+    {
+      href: "https://kyleshevlin.com/comparing-use-ref-and-use-state",
+      linkText: "Comparing `useRef` and `useState` by Kyle Shevlin",
+    },
+  ],
+}
+
+export const TypeScriptResources = {
+  topic: "TypeScript",
+  articles: [
+    {
+      href:
+        "https://egghead.io/lessons/react-write-your-first-react-typescript-component",
+      linkText: "Write your first React TypeScript Component by Shawn Wang",
+    },
+    {
+      href: "https://kentcdodds.com/blog/typescript-function-syntaxes",
+      linkText: "TypeScript Function Syntaxes by Kent C. Dodds",
+    },
+  ],
+}
+
+export const SystemDesignResources = {
+  topic: "System Design",
+  articles: [
+    {
+      href: "https://github.com/donnemartin/system-design-primer",
+      linkText: "The System Design Primer",
+    },
+  ],
+}
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,41 +2,11 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import ResourceList from "../components/ResourceList"
-
-const ReactResources = {
-  topic: "React",
-  articles: [
-    {
-      href: "https://kyleshevlin.com/comparing-use-ref-and-use-state",
-      linkText: "Comparing `useRef` and `useState` by Kyle Shevlin",
-    },
-  ],
-}
-
-const TypeScriptResources = {
-  topic: "TypeScript",
-  articles: [
-    {
-      href:
-        "https://egghead.io/lessons/react-write-your-first-react-typescript-component",
-      linkText: "Write your first React TypeScript Component by Shawn Wang",
-    },
-    {
-      href: "https://kentcdodds.com/blog/typescript-function-syntaxes",
-      linkText: "TypeScript Function Syntaxes by Kent C. Dodds",
-    },
-  ],
-}
-
-const SystemDesignResources = {
-  topic: "System Design",
-  articles: [
-    {
-      href: "https://github.com/donnemartin/system-design-primer",
-      linkText: "The System Design Primer",
-    },
-  ],
-}
+import {
+  ReactResources,
+  TypeScriptResources,
+  SystemDesignResources,
+} from "../articleResources"
 
 const ResourcesPage = () => {
   return (
